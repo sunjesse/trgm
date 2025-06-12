3b8b78a8dcea83b0b52a3c122633d277c90de9ef
sunjesse <jessera@outlook.com>
2025-06-11 23:56:10 -0400

    fmt

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use std::io;
 use std::collections::HashSet;
+use std::io;
 
 /*
 Implementation of postgresql trigram implementation.
@@ -9,14 +9,16 @@ It pads the head with 2 empty spaces, and tail with
 
 fn get_trgm(s: &String) -> Vec<String> {
     /* naive implementation */
-    let mut ans: Vec<String> = Vec::with_capacity((s.len()+4)-2);
+    let mut ans: Vec<String> = Vec::with_capacity((s.len() + 4) - 2);
     let mut _s: String = "  ".to_string();
     _s.push_str(s);
     _s.push_str(" ");
-    for i in 0..=(_s.len()-3) {
-        unsafe { ans.push(_s.get_unchecked(i..(i+3)).to_string()); }
+    for i in 0..=(_s.len() - 3) {
+        unsafe {
+            ans.push(_s.get_unchecked(i..(i + 3)).to_string());
+        }
     }
-    ans 
+    ans
 }
 
 #[inline(always)]
@@ -38,9 +40,11 @@ fn similarity(x: &Vec<String>, y: &Vec<String>) -> f32 {
         let key: u32 = hash(c);
         if let Some(_) = set.get(&key) {
             u += 1;
-        } else { v += 1; }
+        } else {
+            v += 1;
+        }
     }
-    
+
     u as f32 / (v as f32)
 }
 
@@ -56,9 +60,7 @@ fn main() {
     let mut fixed_wrd: String = String::new();
 
     println!("Enter fixed word: ");
-    io::stdin()
-        .read_line(&mut fixed_wrd)
-        .expect("error xd");
+    io::stdin().read_line(&mut fixed_wrd).expect("error xd");
 
     parse(&mut fixed_wrd);
 
@@ -66,9 +68,7 @@ fn main() {
 
     loop {
         let mut x: String = String::new();
-        io::stdin()
-            .read_line(&mut x)
-            .expect("error bruh");
+        io::stdin().read_line(&mut x).expect("error bruh");
         parse(&mut x);
 
         if x == "stop" {
